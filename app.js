168d4db2c4ced01d0968b3c43666a36af412db15
Osuntuyi Amos <osuntuyiemilos@gmail.com>
2022-09-22 17:56:28 +0100

    Update app.js

--- app.js
+++ app.js
@@ -27,7 +27,6 @@ const hiddenElements = document.querySelectorAll(".hidden");
 
 const observer = new IntersectionObserver((entries) => {
   entries.forEach((entry) => {
-    console.log(entry);
     if (entry.isIntersecting) {
       entry.target.classList.add("show");
     }
